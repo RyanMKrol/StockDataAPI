92b3ab313ec6b04f62f4b199c20a921a2ebfffce
Ryan Krol <ryankr@hotmail.co.uk>
2020-03-14 15:06:19 +0000

    Fix no return value on dynamo integration methods

--- src/api/aws/dynamo.ts
+++ src/api/aws/dynamo.ts
@@ -4,25 +4,7 @@ import dynamoCredentials from './../../../credentials/dynamo.json'
 const AWS_REGION = 'us-east-2'
 const dynamoDb = new DynamoDBWrapper(dynamoCredentials, AWS_REGION)
 
-export async function readHeatmapCache() {
-  const table = 'TickerData'
-  const expression = 'dataType = :type'
-  const expressionData = {
-    ':type': 'cache',
-  }
-
-  await dynamoDb.readTable(table, expression, expressionData)
-}
-
-export async function writeHeatmapCache(cacheData: any) {
-  const tableName = 'TickerData'
-  const insertItem = {
-    'itemType': 'cache',
-    'priceData': cacheData,
-  }
-
-  await dynamoDb.writeTable(tableName, insertItem)
-}
+const CACHE_TICKER_ALIAS = 'CACHE'
 
 export async function readTickerTable(ticker: string) {
   const table = 'TickerData'
@@ -31,5 +13,26 @@ export async function readTickerTable(ticker: string) {
     ':ticker': ticker,
   }
 
-  await dynamoDb.readTable(table, expression, expressionData)
+  return await dynamoDb.readTable(table, expression, expressionData)
+}
+
+export async function readHeatmapCache() {
+  const table = 'TickerData'
+  const expression = 'ticker = :ticker'
+  const expressionData = {
+    ':ticker': CACHE_TICKER_ALIAS,
+  }
+
+  return await dynamoDb.readTable(table, expression, expressionData)
+}
+
+
+export async function writeHeatmapCache(cacheData: any) {
+  const tableName = 'TickerData'
+  const insertItem = {
+    'ticker': CACHE_TICKER_ALIAS,
+    'priceData': cacheData,
+  }
+
+  await dynamoDb.writeTable(tableName, insertItem)
 }
