e3da19ae7af2990a0298a0e868fdd7e4cdd7a989
Ryan Krol <ryankr@hotmail.co.uk>
2020-03-14 15:52:43 +0000

    Flesh out HeatMapCache object
    * Will fetch the data on instantiation
    * Will update the cache once a day using data that should have
    been written to the database earlier that day

--- src/cache/heatmap.ts
+++ src/cache/heatmap.ts
@@ -1,17 +1,24 @@
 import schedule from 'node-schedule'
 import async from 'async'
-import { readTickerTable } from './../api/aws'
+
+import {
+  readTickerTable,
+  readHeatmapCache,
+  writeHeatmapCache,
+} from './../api/aws'
 import { fetchIndexTickers } from './../api/stocktickersapi'
-import { processHeatmapCacheItem } from './../process'
-import { wait } from './../utils'
+import {
+  processPriceDataForCacheWrite,
+  processDataForCacheRead,
+} from './../process'
+import { wait, sendMail } from './../utils'
 
 // configuring the seconds wait to keep consumption of read credits low
 const SECONDS_WAIT = 10
 const MS_IN_S = 1000
 
-// requests are done one at a time to reduce the amount of writes per second going to dynamo
-const MAX_CONCURRENT_REQUESTS = 1
-
+// requests are done in batches to reduce the amount of reads per second going to dynamo
+const MAX_CONCURRENT_REQUESTS = 10
 
 class HeatMapCache {
   private static instance: HeatMapCache
@@ -29,43 +36,49 @@ class HeatMapCache {
     return HeatMapCache.instance
   }
 
-  private fetchCurrentCache() {
-    // const currentCacheData = readHeatmapCache
-    console.log('fetching the current cache data')
-    console.log(this.data)
-    // this.data = currentCacheData
+  private async fetchCurrentCache() {
+    try {
+      const currentCacheData = await readHeatmapCache()
+      this.data = processDataForCacheRead(currentCacheData)
+    } catch (error) {
+      sendMail('Failed to read heatmap cache', error.toString())
+    }
   }
 
   private async scheduleCacheUpdate() {
-    schedule.scheduleJob('0 0 16 * * *', async () => {
-    })
     const self = this
-    const tickers = (await fetchIndexTickers()).slice(0,2)
-    console.log(tickers)
-    console.log('starting the work of fetching everytthing')
-    await new Promise((resolve, reject) => {
-      async.mapLimit(tickers, MAX_CONCURRENT_REQUESTS, async function(ticker: string, callback: Function) {
-        try {
-          const tickerData = await readTickerTable(ticker)
-          const dataEntry = processHeatmapCacheItem(tickerData)
 
-          self.data[ticker] = dataEntry
-          console.log('starting wait')
-          await wait(SECONDS_WAIT*MS_IN_S)
-          console.log('finished wait')
+    schedule.scheduleJob('0 0 16 * * *', async () => {
+      await sendMail('Starting the hearmap cache update for the day!', '')
 
-          callback()
-        } catch (error) {
-          const errorMessage =
-            `Encountered an error when gathering data for ticker - ${ticker}, error = ${error.toString()}`
-          console.log(errorMessage)
-          callback()
-        }
-      }, (err: any, results: any) => {
-        resolve()
+      const tickers = await fetchIndexTickers()
+
+      await new Promise((resolve, reject) => {
+        async.mapLimit(tickers, MAX_CONCURRENT_REQUESTS, async function(ticker: string, callback: Function) {
+          try {
+            // wait at the beginning in case anything goes wrong some point in the middle
+            await wait(SECONDS_WAIT*MS_IN_S)
+
+            const tickerData = await readTickerTable(ticker)
+            const dataEntry = processPriceDataForCacheWrite(tickerData)
+
+            self.data[ticker] = dataEntry
+
+            callback()
+          } catch (error) {
+            const errorMessage =
+              `Encountered an error when gathering data for ticker - ${ticker}, error = ${error.toString()}`
+            sendMail('Failed to read data for cache update!', errorMessage)
+            callback()
+          }
+        }, (err: any, results: any) => {
+          resolve()
+        })
       })
+
+      await writeHeatmapCache(self.data)
+      await sendMail('Successfully updated the heatmap cache for the day!', '')
     })
-    console.log('finished the work of fetching everytthing')
   }
 }
 
