9bd33f6bd92df363744a9dde8f6e0e0312a502f2
Ryan Krol <ryankr@hotmail.co.uk>
2020-03-14 14:57:52 +0000

    Update dynamo integration methods to be asynchronous

--- src/api/aws/dynamo.ts
+++ src/api/aws/dynamo.ts
@@ -4,32 +4,32 @@ import dynamoCredentials from './../../../credentials/dynamo.json'
 const AWS_REGION = 'us-east-2'
 const dynamoDb = new DynamoDBWrapper(dynamoCredentials, AWS_REGION)
 
-export function readHeatmapCache() {
+export async function readHeatmapCache() {
   const table = 'TickerData'
   const expression = 'dataType = :type'
   const expressionData = {
     ':type': 'cache',
   }
 
-  return dynamoDb.readTable(table, expression, expressionData)
+  await dynamoDb.readTable(table, expression, expressionData)
 }
 
-export function writeHeatmapCache(cacheData: any) {
+export async function writeHeatmapCache(cacheData: any) {
   const tableName = 'TickerData'
   const insertItem = {
     'itemType': 'cache',
     'priceData': cacheData,
   }
 
-  return dynamoDb.writeTable(tableName, insertItem)
+  await dynamoDb.writeTable(tableName, insertItem)
 }
 
-export function readTickerTable(ticker: string) {
+export async function readTickerTable(ticker: string) {
   const table = 'TickerData'
   const expression = 'ticker = :ticker'
   const expressionData = {
     ':ticker': ticker,
   }
 
-  return dynamoDb.readTable(table, expression, expressionData)
+  await dynamoDb.readTable(table, expression, expressionData)
 }
