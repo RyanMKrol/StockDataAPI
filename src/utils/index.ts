d09c4d132285689501fa33d627a1fbcc69d286c4
Ryan Krol <ryankr@hotmail.co.uk>
2020-03-14 15:22:47 +0000

    Add mailer util

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,1 +1,2 @@
 export { wait } from './wait'
+export { default as sendMail } from './mailer'
---
+++ src/utils/mailer.ts
@@ -0,0 +1,12 @@
+import MailSender from 'noodlesmail'
+import gmailCredentials from './../credentials/gmail.json'
+
+const mailClient = new MailSender(gmailCredentials)
+mailClient.setFrom('"StockPriceDataAPI" <ryankrol.m@gmail.com>')
+mailClient.setTo('ryankrol.m@gmail.com')
+
+async function sendMail(subject: string, body: string) {
+  await mailClient.sendMail(subject, body)
+}
+
+export default sendMail
