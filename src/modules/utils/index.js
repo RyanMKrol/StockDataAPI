818b9b6c5472d0b29c59d768b3e13235a2532e23
Ryan Krol <ryankr@hotmail.co.uk>
2020-09-20 22:55:40 +0100

    Refactor date methods to return moment object instead of string
    * This should keep the dates abstracted to just the one
    file, which can then be switched out at any time as necessary

--- src/modules/utils/index.js
+++ src/modules/utils/index.js
@@ -1,19 +1,48 @@
+/**
+ * @module utils
+ */
+
 import moment from 'moment';
 import { SUPPORTED_TIME_PERIODS } from '../constants';
+
+/**
+ * The default value from the moment import
+ *
+ * @typedef Moment
+ */
 
 /**
  * Gets the date indicated by the request to the API
  *
  * @param {string} timePeriod The time period passed in from the request
- * @returns {string} The date of the price we want to fetch for our tickers
+ * @returns {Moment} The date of the price we want to fetch for our tickers
  */
 function fetchTargetHeatmapDate(timePeriod) {
   const daysToRemove = SUPPORTED_TIME_PERIODS[timePeriod];
   const date = moment();
 
-  date.subtract(daysToRemove, 'days');
+  return date.subtract(daysToRemove, 'days');
+}
 
+/**
+ * Gets the "todays" date to fetch heatmap data for. This will always be
+ * one day behind as there's no guarantee that the price data API will have
+ * run today yet
+ *
+ * @returns {Moment} "Todays" date we want to compare our heatmap data with
+ */
+function fetchTodayHeatmapDate() {
+  return moment().subtract(1, 'days');
+}
+
+/**
+ * Small method to format date according to the Heatmap spec
+ *
+ * @param {Moment} date The date to format
+ * @returns {string} The date formatted for heatmaps
+ */
+function formatHeatmapDate(date) {
   return date.format('YYYY-MM-DD');
 }
 
-export default fetchTargetHeatmapDate;
+export { fetchTargetHeatmapDate, fetchTodayHeatmapDate, formatHeatmapDate };
