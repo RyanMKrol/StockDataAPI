06c71ce0c4a4def43f8bf68f8e46199b7c7228ea
Ryan Krol <ryankr@hotmail.co.uk>
2020-09-20 14:34:06 +0100

    Add constants module

---
+++ src/modules/constants/index.js
@@ -0,0 +1,11 @@
+/** @module constants */
+
+const SUPPORTED_TIME_PERIODS = {
+  one_month: 30,
+  three_month: 90,
+  six_month: 180,
+  one_year: 360,
+  two_year: 720,
+};
+
+export default SUPPORTED_TIME_PERIODS;
