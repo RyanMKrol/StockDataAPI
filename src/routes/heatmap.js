b677976193a7d36f5e70ff809f6415bf5a869801
Ryan Krol <ryankr@hotmail.co.uk>
2020-09-21 00:40:28 +0100

    Flesh out heatmap route
    * Calls the heatmap data schedule to ensure that the data
    is updated.
    * Start using the central datastore to fetch the response

--- src/routes/heatmap.js
+++ src/routes/heatmap.js
@@ -1,6 +1,13 @@
 import express from 'express';
 
-import processRequest from '../modules/request';
+import scheduleHeatmapDataUpdates from '../modules/schedules';
+import dataStore from '../modules/data';
+import { validateHeatmapsRequestTimePeriod } from '../modules/utils';
+import { MissingCacheData } from '../modules/errors';
+
+// When this route is created, we need to setup the schedule that
+// provides data to it
+scheduleHeatmapDataUpdates();
 
 const router = express.Router();
 
@@ -8,10 +15,21 @@ const router = express.Router();
 router.use('/:timePeriod', async (req, res, next) => {
   const { timePeriod } = req.params;
 
-  const request = await processRequest(timePeriod);
+  try {
+    validateHeatmapsRequestTimePeriod(timePeriod);
+    const data = dataStore.getData(timePeriod);
 
-  console.log(request);
-  res.send('Doing just fine');
+    if (!data) {
+      throw new MissingCacheData();
+    }
+
+    res.send(data);
+  } catch (err) {
+    res.status(err.StatusCode || 500);
+    res.send({
+      error: err.Error,
+    });
+  }
 });
 
 export default router;
