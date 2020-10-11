ec030f048d2f218232b6b6bc86383a72ab8a787c
Ryan Krol <ryankr@hotmail.co.uk>
2020-10-11 17:37:19 +0100

    Move indexes data to constants file

--- src/modules/constants/index.js
+++ src/modules/constants/index.js
@@ -3,6 +3,7 @@ import GMAIL_CREDENTIALS from '../../../credentials/gmail.json';
 
 const DYNAMO_REGION = 'us-east-2';
 const DATA_STORE_KEY_HEATMAPS = 'heatmaps';
+const DATA_STORE_KEY_TICKERS = 'tickers';
 
 const SUPPORTED_TIME_PERIODS = {
   one_month: 30,
@@ -12,10 +13,21 @@ const SUPPORTED_TIME_PERIODS = {
   two_year: 720,
 };
 
+const SUPPORTED_INDEXES = {
+  ftse_100: 'https://www.londonstockexchange.com/indices/ftse-100/constituents/table',
+  ftse_250: 'https://www.londonstockexchange.com/indices/ftse-250/constituents/table',
+  ftse_350: 'https://www.londonstockexchange.com/indices/ftse-350/constituents/table',
+  ftse_all_share: 'https://www.londonstockexchange.com/indices/ftse-all-share/constituents/table',
+  ftse_small_cap:
+    'https://www.londonstockexchange.com/indices/ftse-aim-all-share/constituents/table',
+};
+
 export {
   SUPPORTED_TIME_PERIODS,
+  SUPPORTED_INDEXES,
   DYNAMO_CREDENTIALS,
   GMAIL_CREDENTIALS,
   DYNAMO_REGION,
   DATA_STORE_KEY_HEATMAPS,
+  DATA_STORE_KEY_TICKERS,
 };
